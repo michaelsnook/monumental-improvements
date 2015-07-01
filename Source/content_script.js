70adfc555b36b12b69100722bec9c391f3763ddc
michaelsnook <michael.snook@gmail.com>
2015-07-01 14:07:17 -0400

    first commit of new functionality

--- Source/content_script.js
+++ Source/content_script.js
@@ -1,19 +1,19 @@
 walk(document.body);
 
-function walk(node) 
+function walk(node)
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
+
 	var child, next;
 
-	switch ( node.nodeType )  
+	switch ( node.nodeType )
 	{
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
+			while ( child )
 			{
 				next = child.nextSibling;
 				walk(child);
@@ -27,16 +27,14 @@ function walk(node)
 	}
 }
 
-function handleText(textNode) 
+function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+	v = v.replace(/\bVandals defaced\b/g, "Heroes improved");
+	v = v.replace(/\bdefaced\b/g, "improved");
+	v = v.replace(/\bDefaced\b/g, "Improved");
+	v = v.replace(/\bvandalized\b/g, "corrected");
+
 	textNode.nodeValue = v;
 }
-
-

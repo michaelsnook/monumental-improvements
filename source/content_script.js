77ecdbea4704c5c06b57f718ddde6d938484a0c0
michaelsnook <michael.snook@gmail.com>
2015-08-15 16:35:03 -0400

    updates to include variations of 'political correctness'

--- source/content_script.js
+++ source/content_script.js
@@ -37,6 +37,14 @@ function handleText(textNode)
 	v = v.replace(/\bdefaced\b/g, "improved");
 	v = v.replace(/\bDefaced\b/g, "Improved");
 	v = v.replace(/\bvandalized\b/g, "corrected");
+	v = v.replace(/\bpolitical correctness\b/g, "treating others with respect");
+	v = v.replace(/\bPolitical correctness\b/g, "Treating others with respect");
+	v = v.replace(/\bpolitically correct\b/g, "respectful of others");
+	v = v.replace(/\bPolitically correct\b/g, "Respectful of others");
+	v = v.replace(/\bPolitically Correct\b/g, "Respectful of Others");
+	v = v.replace(/\bPC police\b/g, "Respectful people");
+	v = v.replace(/\bPC-police\b/g, "Respectful people");
+	v = v.replace(/\bun-PC\b/g, "shitty to people");
 
 	textNode.nodeValue = v;
 }
